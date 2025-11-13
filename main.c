d80a1994fe73c669e8dad255481e33e9c05dd8b0
yunjin25-hue <yunjin25@sookmyung.ac.kr>
2025-11-13 15:23:05 +0900

    11

--- main.c
+++ main.c
@@ -3,21 +3,14 @@
 
 int main(int argc, char *argv[])
 {
+  //실습 2
   
-  int i=10;
-  char c='a';
+  int i=300; 
   
-  int *iptr;
-  char *cptr;
-  int *iptr2;
+  int *pi=&i;
+  char *pc=&i;
   
-  iptr=&i;
-  cptr=&c;
-  iptr2=iptr; //변수 안에 이미 주소 들어가 있어서 & 안붙여도 됨 
-  
-  printf("i:%p\n%p(size:%i)\n",iptr,&i,sizeof(iptr));
-  printf("c:%p\n%p(size:%i)\n",cptr,&c,sizeof(cptr));
-  printf("iptr2:%p,%i\n",iptr2,*iptr2);
+  printf("%i, %i, %i\n",i,*pi,*pc);
   
   system("PAUSE");	
   return 0;
